e70b4e972539631e0842d991fa7616d9b5572c0e
mattreed09 <matthew_reed@live.co.uk>
2018-08-31 16:56:11 +0100

    add context to agnostic

--- pattern matching/AgnosticPatternMatching.R
+++ pattern matching/AgnosticPatternMatching.R
@@ -1,5 +1,15 @@
 
 
+# with a set of patterns and names in a data frame names rules, and a vector of strings called strings,
+# this code will produce a data frame with the regex and name and all the strings it matches with for 
+# every match between the rules and the strings.
+
+library(plyr)
+library(stringr)
+
+rules <- rules
+strings = df2$uri 
+
 rules$row = row.names(rules)
 m<- merge(strings, rules, stringsAsFactors=F)
 m[]<-lapply(m,as.character)
